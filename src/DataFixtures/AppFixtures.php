c152c3434b6072a788c2bc266d892f926bc0427a
jean-christophe Fauquenot <jcfauquenot@gmail.com>
2019-11-21 13:56:35 +0100

    update fixture with encoder password

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,14 +5,23 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class AppFixtures extends Fixture
 {
+private $encoder;
+
+public function __construct(UserPasswordEncoderInterface $encoder)
+{
+    $this->encoder = $encoder;
+}
     public function load(ObjectManager $manager)
     {
         $user  = new User();
         $user->setEmail('jcfauquenot@gmail.com');
-        $user->setPassword('best');
+        $password = $this->encoder->encodePassword($user, 'pass_1234');
+        $user->setPassword($password);
         $user->setRoles(['admin']);
 
         $manager->persist($user);
